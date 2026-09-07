7003eb7b7d7bb200496a7b20e75e3d47fcaeaa1f
ImagineBeam <wielogorkab@gmail.com>
2026-09-07 15:00:33 +0200

    Basic functionality
    App is now able to dynamically change table based on chosen tier of material.
    Also it is able to calculate quantity of each material based on capacity.

---
+++ script.js
@@ -0,0 +1,170 @@
+let startButton = document.getElementById("startButton")
+let materialEl = document.getElementById("material")
+let tierEl = document.getElementById("tier")
+let levelEl = document.getElementById("level")
+let weightEl = document.getElementById("weight")
+let capitalEl = document.getElementById("capital")
+let calculateByCapital = false
+let sheet = document.getElementById("sheet")
+const AlbionMaterials = {
+    2: {raw: 1, weight: 0.23},
+    3: {raw: 2, weight: 0.34},
+    4: {raw: 2, weight: 0.51},
+    5: {raw: 3, weight: 0.76},
+    6: {raw: 4, weight: 1.14},
+    7: {raw: 5, weight: 1.7},
+    8: {raw: 5, weight: 2.6}
+}
+
+function start()
+{
+    let material = materialEl.value
+    let tier = tierEl.value
+    let level = levelEl.value
+
+
+    if(document.getElementById("max_capital").checked)
+    {
+        calculateByCapital = true
+        capitalOrWeight = capitalEl.value
+    }
+    else
+    {
+        calculateByCapital = false
+        capitalOrWeight = weightEl.value
+    }
+    
+    
+    calculationSheet(material, tier, level, calculateByCapital, capitalOrWeight)
+}
+    
+
+startButton.addEventListener("click", () => start())
+
+function calculationSheet(material, tier, level, calculateByCapital, capitalOrWeight)
+{
+    // material is only for visuals (it doesn't affect calculations, just prices in the future with API)
+    // level only affects price with API
+    let tbl = document.createElement("table")
+    let tbdy = document.createElement("tbody")
+    for(let i=0; i<3; i++)
+    {
+        let tr = document.createElement("tr")
+        if(i==0)
+        {
+            tierTable(tr, tier)
+        }
+        else if(i==1)
+        {
+            priceTable(tr, tier)
+        }
+        else if(i==2)
+        {
+            quantityTable(tr, tier)
+        }
+        tbdy.appendChild(tr)
+    }
+    tbl.appendChild(tbdy)
+    sheet.innerHTML = ""
+    sheet.appendChild(tbl)
+    let button = document.createElement("button")
+    button.setAttribute("id", "real_calculate")
+    button.innerHTML = "Calculate profit!"
+    sheet.appendChild(button)
+    document.getElementById("real_calculate").addEventListener("click", () => profit())
+}
+
+function tierTable(tr, tier)
+{
+    for(let j=1; j<=tier; j++)
+    {
+        let td = document.createElement("td")
+        if(j==1)
+        {
+            td.appendChild(document.createTextNode(""))
+            tr.appendChild(td)
+        }
+        else
+        {
+            td.appendChild(document.createTextNode("T"+j))
+            tr.appendChild(td)
+        }
+    }
+}
+
+function priceTable(tr, tier)
+{
+    for(let j=1; j<=tier; j++)
+    {
+        let td = document.createElement("td")
+        if(j==1)
+        {
+            td.appendChild(document.createTextNode("Price"))
+            tr.appendChild(td)
+        }
+        else
+        {
+            let input = document.createElement("input")
+            input.setAttribute("id", "t"+j+"_price")
+            td.appendChild(input)
+            tr.appendChild(td)
+        }
+    }
+}
+
+function quantityTable(tr, tier)
+{
+    for(let j=1; j<=tier; j++)
+    {
+        let td = document.createElement("td")
+        if(j==1)
+        {
+            td.appendChild(document.createTextNode("Quantity"))
+            tr.appendChild(td)
+        }
+        else
+        {
+            let p = document.createElement("p")
+            p.setAttribute("id", "t"+j+"_quantity")
+            td.appendChild(p)
+            tr.appendChild(td)
+        }
+    }
+}
+
+function profit()
+{
+    let tier = tierEl.value
+    let max_weight = weightEl.value /0.7
+    let divider = 0
+    let R = 1 - 0.367
+    for (let i = tier; i > 1; i--)
+    {
+        if(i==tier)
+        {
+            divider += AlbionMaterials[i].raw * AlbionMaterials[i].weight
+        }
+        else
+        {
+            divider += AlbionMaterials[i].raw * AlbionMaterials[i].weight * Math.pow(R, tier - i)
+        }
+        console.log(divider)
+    }
+    let how_many = max_weight/divider
+    console.log(how_many)
+    for (let i = tier; i > 1; i--)
+    {
+        let this_many = 0
+        if(i==tier)
+        {
+            this_many = how_many * AlbionMaterials[i].raw
+        }
+        else
+        {
+            this_many = how_many * AlbionMaterials[i].raw * Math.pow(R, tier - i)
+        }
+        document.getElementById("t"+i+"_quantity").textContent = this_many
+        console.log(tier-i)
+    }
+}
+
